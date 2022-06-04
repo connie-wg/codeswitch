85447fee23d408b9b7ee7f901bc5c1312e062745
Jenna Xiao <jenna_xiao@hotmail.com>
2022-06-04 12:44:41 -0400

    adding server

---
+++ flask-server/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# example api
+@app.route("/members")
+def members():
+  return {"members": ["Member1", "Member2", "Member3"]} # random stuff
+
+if __name__ == "__main__":
+  app.run(debug=True) # since this is in development mode
