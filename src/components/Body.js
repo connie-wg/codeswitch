79a39ede1f43d307ab1bc2742789bf7c7816fb9d
Jenna Xiao <jenna_xiao@hotmail.com>
2022-06-04 14:38:03 -0400

    check apis

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,24 @@
 import Grid from '@mui/material/Grid';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import React, { useState, useEffect } from 'react'
 
 function Text() {
+  const [data, setData] = useState([{}]);
+  const [code, setCode] = useState('');
+  const [engCode, setEngCode] = useState('');
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
     <CodeMirror
       value="console.log('hello world!');"
